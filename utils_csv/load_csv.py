a9c3e7f810c6633c7f76953a8abe3f75c1487147
ngel rojas <aomarrojasp@gmail.com>
2023-04-12 16:48:30 -0300

    add: refactor function and create packages

---
+++ utils_csv/load_csv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+class LoadCsv:
+
+    def __init__(self, origen_csv):
+        self.origen_csv = origen_csv
+
+    def create_load_csv(self, output_csv, node_id, node_label) -> None:
+        df = pd.read_csv(self.origen_csv)
+        df = df[[node_id, node_label]]
+        df.dropna(subset=[node_id, node_label], inplace=True)
+        df.to_csv(output_csv, index=False)
+        return None
